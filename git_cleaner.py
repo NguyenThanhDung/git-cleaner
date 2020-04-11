ecbfb95810dea870f80b1312d05428d206aef089
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-04-11 18:04:26 +0700

    Print all available branches

--- git_cleaner.py
+++ git_cleaner.py
@@ -1,2 +1,13 @@
+import subprocess
+
+
+def ListBranches():
+    subprocess.call(["git", "branch"])
+
+
+def Main():
+    ListBranches()
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    Main()
