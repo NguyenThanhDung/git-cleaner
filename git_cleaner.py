868e933f927d41dc98bbe1feed94e82cf477e507
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-08-11 10:19:15 +0700

    Get all branches and print one by one

--- git_cleaner.py
+++ git_cleaner.py
@@ -1,13 +1,23 @@
 import subprocess
+import sys
 
 
-def ListBranches():
-    subprocess.call(["git", "branch"])
+def list_branches():
+    # subprocess.call(["git", "branch"])
+    command = ["git", "branch"]
+    p = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if p.stderr:
+        print("error:", p.stderr, file=sys.stderr)
+    else:
+        branches = p.stdout.split()
+        branches.remove(b"*")
+        for branch in branches:
+            print(branch)
 
 
-def Main():
-    ListBranches()
+def main():
+    list_branches()
 
 
 if __name__ == "__main__":
-    Main()
+    main()
