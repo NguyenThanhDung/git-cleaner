f443e27cdb9165b619070fdbe0aaf7da0c7f0af9
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2020-09-13 17:40:57 +0700

    Adjust code to be compatable with Python 2.7

--- git_cleaner.py
+++ git_cleaner.py
@@ -1,15 +1,14 @@
-import subprocess
-import sys
+from subprocess import Popen, PIPE
 
 
 def list_branches():
-    # subprocess.call(["git", "branch"])
     command = ["git", "branch"]
-    p = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if p.stderr:
-        print("error:", p.stderr, file=sys.stderr)
+    process = Popen(command, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if stderr:
+        print("error:", stderr)
     else:
-        branches = p.stdout.split()
+        branches = stdout.split()
         branches.remove(b"*")
         for branch in branches:
             print(branch)
